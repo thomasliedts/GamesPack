8bd1af1f4056becbf6d16484a2926436281e3b0b
Thomas Liedts <thomasliedts@gmail.com>
2019-11-04 13:53:03 +0100

    routing fait et component créé

---
+++ src/app/inscript/inscript.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inscript',
+  templateUrl: './inscript.component.html',
+  styleUrls: ['./inscript.component.scss']
+})
+export class InscriptComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
