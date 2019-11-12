60316c8b609b5df2ccbb8583b7c6ecb62a23d2e7
Thomas Liedts <thomasliedts@gmail.com>
2019-11-12 14:51:28 +0100

    connec api done, register presque fini

--- src/app/inscript/inscript.component.ts
+++ src/app/inscript/inscript.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
+import{inscription} from '../model/user'
+import { Observable } from 'rxjs';
+import { UsersService } from '../users.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inscript',
@@ -6,10 +11,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inscript.component.scss']
 })
 export class InscriptComponent implements OnInit {
-
-  constructor() { }
+  inscription ={
+    pseudo:'',
+    email:'',
+    password:'',
+  };
+  creationForm:FormGroup;
+  constructor(private router: Router, private usersService: UsersService, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.createFormForUser();
   }
-
+  createFormForUser() {
+    this.creationForm = this.fb.group({
+      name: '',
+      email: '',
+      password: '',
+    });
+  }
+  CreateUserNew() {
+    if (this.creationForm.valid) {
+      console.log('good job', this.creationForm);
+      this.usersService.createUser(this.creationForm.value).subscribe((data) => {
+        console.log(data, 'User créer');
+      });
+    }
+  }
 }
