e0a7451023f00687638f89b748c146edd353dadf
Thomas Liedts <thomasliedts@gmail.com>
2019-11-22 15:09:08 +0100

    reboot de jwt

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -5,36 +5,6 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
-  login(email:string, password:string ) {
-    return this.http.post<User>('/api/login', {email, password})
-        // this is just the HTTP call, 
-        // we still need to handle the reception of the token
-        .shareReplay();
-  }
-  private setSession(authResult) {
-    const expiresAt = moment().add(authResult.expiresIn,'second');
+  constructor() { }
 
-    localStorage.setItem('id_token', authResult.idToken);
-    localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()) );
-}          
-
-logout() {
-    localStorage.removeItem("id_token");
-    localStorage.removeItem("expires_at");
-}
-
-public isLoggedIn() {
-    return moment().isBefore(this.getExpiration());
-}
-
-isLoggedOut() {
-    return !this.isLoggedIn();
-}
-
-getExpiration() {
-    const expiration = localStorage.getItem("expires_at");
-    const expiresAt = JSON.parse(expiration);
-    return moment(expiresAt);
-} 
 }
